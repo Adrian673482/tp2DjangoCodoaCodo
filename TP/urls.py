587fa29d8be2add52b66a43fffdbd6c98d210f4c
AdriangelesRodri <115745553+Adrian673482@users.noreply.github.com>
2023-12-21 19:16:28 -0300

    agregar proyecto

--- TP/urls.py
+++ TP/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('TIENDA.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('USUARIOS/', include('USUARIOS.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('favicon.ico', lambda x: HttpResponseNotFound())
+]
